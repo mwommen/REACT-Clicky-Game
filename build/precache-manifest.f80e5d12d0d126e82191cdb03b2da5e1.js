5433ed4266fed76ae3aab1dfc37f6a537c377193
Mike O <mwommen@gmail.com>
2019-10-24 11:19:28 -0500

    Adding React template

---
+++ build/precache-manifest.f80e5d12d0d126e82191cdb03b2da5e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70db83b441bd769bba59a0c2cda51417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2088ac47a4a2f1992e",
+    "url": "/static/css/main.31042cad.chunk.css"
+  },
+  {
+    "revision": "d199873d93bb19587acd",
+    "url": "/static/js/2.2a29509a.chunk.js"
+  },
+  {
+    "revision": "3d2088ac47a4a2f1992e",
+    "url": "/static/js/main.57640c3b.chunk.js"
+  },
+  {
+    "revision": "0ab870c873fedbf19b0e",
+    "url": "/static/js/runtime-main.74d4fdea.js"
+  }
+]);
